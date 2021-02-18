778045e52a9ff332950de5dc48895cddf7d8b8ca
Omar Magdy <contact.parmag@gmail.com>
2021-02-18 17:30:29 +0200

    App.js:add:axios request

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+const axios = require('axios').default;
 
 function App() {
+
+
+axios({
+  method: 'post',
+  url: 'http://127.0.0.1:5000/users/login/test',
+  crossDomain: true
+})
+.then(function (response) {
+    console.log(response);
+    console.log("Made it");
+  })
+.catch(function (error){
+  console.log("Something went wrong")
+});
+
+
   return (
     <div className="App">
       <header className="App-header">
