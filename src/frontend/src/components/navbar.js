61630a39229e7e958597a6c9291c14cdc6214a5c
Omar Magdy <contact.parmag@gmail.com>
2021-02-20 21:50:50 +0200

    navbar.js:add:render

--- src/frontend/src/components/navbar.js
+++ src/frontend/src/components/navbar.js
@@ -1,7 +1,20 @@
 import {Component} from "react";
 
-class Navbar {
-  constructor() {
+class Navbar extends Component {
 
+  render = () => {
+    const logged_in = this.probe.logged_in;
+    if(logged_in){
+      return (
+        <div>
+          <p>The user is logged in</p>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <p>The user is not logged in</p>
+      </div>
+    )
   }
 }
