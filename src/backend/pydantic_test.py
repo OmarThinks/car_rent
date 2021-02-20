1ac36c063c0752e9de17dcb844eda6e8ad365064
Omar Magdy <contact.parmag@gmail.com>
2021-02-20 18:37:04 +0200

    pydantic_test.py:add:prepare to test login

--- src/backend/pydantic_test.py
+++ src/backend/pydantic_test.py
@@ -1,6 +1,6 @@
 import unittest
-from pydantic_models import (UserPost, UserUpdatePassword, #ProductPost,
-#OrderPost, OrderUpdate,
+from pydantic_models import (UserPost, UserUpdatePassword, UserLogin,
+#ProductPost,#OrderPost, OrderUpdate,
 validate_model_id,
 validate_model_id_pydantic, TestHere)
 import json
@@ -260,6 +260,98 @@ class pydanticTestCase(unittest.TestCase):
 
 
 
+
+
+
+
+
+	def test_b_001_01_1_UserPost(self):
+		toValidate = {"username":123,"password1":7890123456,"password2":"7890123456"}
+		user = UserPost(**toValidate)
+		self.assertEqual(user.dict(),{"username":"123","password1":"7890123456",
+			"password2":"7890123456"})
+		print("Test b_1_1_1:UserPost Successful")
+
+	def test_b_001_01_2_UserPost(self):
+		toValidate = {}
+		try:
+			user = UserPost(**toValidate)
+			self.assertEqual(True,False)
+		except Exception as e:
+			#print(json.loads(e.json()))
+			self.assertEqual(json.loads(e.json()),[{"loc": ["username"],
+				"msg": "field required","type": "value_error.missing"},
+				{"loc": ["password1"],"msg": "field required",
+				"type": "value_error.missing"
+				},{"loc": ["password2"],"msg": "field required",
+				"type": "value_error.missing"
+				}])
+		print("Test b_1_1_2:UserPost:Fail:all missing required")
+
+	def test_b_001_01_3_UserPost(self):
+		toValidate = {"password1":{},"username":{},"password2":{}}
+		try:
+			user = UserPost(**toValidate)
+			self.assertEqual(True,False)
+		except Exception as e:
+			#print(json.loads(e.json()))
+			self.assertEqual(json.loads(e.json()),[{"loc": ["username"],
+				"msg": "str type expected","type": "type_error.str"},{"loc": [
+				"password1"],"msg": "str type expected","type": "type_error.str"
+				},{"loc": ["password2"],"msg": "str type expected",
+				"type": "type_error.str"}])
+		print("Test b_1_1_3:UserPost:Fail:not string")
+
+
+	def test_b_001_01_4_UserPost(self):
+		# username contains spaces
+		# password mismatch
+		# passwords lebgth less than 8
+		# Note, did not notice password mismatch,
+		# because password 1 did not pass the validation
+		toValidate = {"username":"My Name","password1":"123","password2":"789"}
+		try:
+			user = UserPost(**toValidate)
+			self.assertEqual(True,False)
+		except Exception as e:
+			# print(json.loads(e.json()))
+			self.assertEqual(json.loads(e.json()),[{"loc": ["username"],
+				"msg": "username should not contain a space",
+				"type": "value_error"},{"loc": ["password1"],
+				"msg": "ensure this value has at least 5 characters",
+				"type": "value_error.any_str.min_length","ctx": {
+				"limit_value": 5}},{"loc": ["password2"],
+				"msg": "ensure this value has at least 5 characters",
+				"type": "value_error.any_str.min_length","ctx": {
+				"limit_value": 5}}])
+		print("Test b_1_1_4:UserPost:Fail:username contains spaces, short password")
+
+	def test_b_001_01_5_UserPost(self):
+		# password mismatch
+		# Username already exists
+		toValidate = {"username":"abc","password1":"123456789999999000000000",
+		"password2":"12345678"}
+		try:
+			user = UserPost(**toValidate)
+			self.assertEqual(True,False)
+		except Exception as e:
+			#print(json.loads(e.json()))
+			self.assertEqual(json.loads(e.json()),[{
+				'loc': ['username'], 'msg': 'this username already exists',
+				'type': 'value_error'}, {'loc': ['password2'], 'msg':
+				'passwords do not match', 'type': 'value_error'}])
+		print("Test b_1_1_5:UserPost:Fail:password mismatch")
+
+	def test_b_001_01_6_UserPost(self):
+		# adding unknown attribute
+		# This attribute will not be returned
+		# Testing White spaces
+		toValidate = {"username":" MyName ","password1":"12345678",
+		"password2":"12345678", "unknown":"abc"}
+		user = UserPost(**toValidate)
+		self.assertEqual(user.dict(),{"username":"MyName","password1":"12345678",
+		"password2":"12345678"})
+		print("Test b_1_1_6:UserPost:Added unknown value:Cleaned")
 
 
 
